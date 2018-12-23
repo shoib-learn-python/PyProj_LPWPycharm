be4442b6df6d46964ed30037974f302d319f8800
Ansari <mohd_shoib.ansari.ext@nokia.com>
2018-12-24 01:34:54 +0530

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path):
+    #TODO: Genereate the database
+    pass
+
+def save_words_to_database(database_path, word_list):
+    #TODO : save the words to database
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def  load_url_from_file(file_path):
+    #TODO: add the code needed to read the txt file with urls in it
+    pass
+
+def load_page(url):
+    #TODO : add the code that reads url
+    pass
+
+def scrape_page(page_contents):
+    #TODO: analyze the text
+    pass
